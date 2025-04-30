65893bbf4b03db513d5b9c20d3c7cf1f9fc11f2d
Peter Eriksson <tuvoc1@hotmail.com>
2025-04-30 02:58:39 +0200

    Kicker refactored

--- components/Kicker.tsx
+++ components/Kicker.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+const techIcons = [
+  {
+    src: "/kicker-tech-icons/react.png",
+    alt: "React.js icon",
+    label: "React.js",
+  },
+  {
+    src: "/kicker-tech-icons/next.png",
+    alt: "Next.js icon",
+    label: "Next.js",
+  },
+  {
+    src: "/kicker-tech-icons/typescript.png",
+    alt: "TypeScript icon",
+    label: "TypeScript",
+  },
+  {
+    src: "/kicker-tech-icons/tw.png",
+    alt: "Tailwind CSS icon",
+    label: "Tailwind CSS",
+  },
+];
+
 export default function Kicker() {
   return (
-    <motion.p
-      initial={{
-        opacity: 0,
-        y: 10,
-      }}
-      transition={{
-        duration: 0.7,
-        delay: 1.2,
-      }}
-      whileInView={{
-        opacity: 0.7,
-        y: 0,
-      }}
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      transition={{ duration: 0.7, delay: 1.2 }}
+      whileInView={{ opacity: 0.7, y: 0 }}
       viewport={{ once: true }}
       className="hidden sm:inline whitespace-nowrap absolute -top-12 text-kicker
                  font-bold text-react/90 tracking-wide"
@@ -23,63 +37,24 @@ export default function Kicker() {
       React
       <span className="text-white/40">
         ing to Your UI Needs with
-        <div className="inline-flex items-center space-x-2 ml-2 alignHorizontal: transform translate-y-1">
-          <div className="relative group">
-            <Image
-              src="/react.png"
-              alt="React.js icon"
-              width={20}
-              height={20}
-              objectFit="contain"
-              className=""
-            />
-            <div className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100 bg-black text-white text-xs rounded px-2 py-1 -top-8">
-              React.js
+        <div className="inline-flex items-center space-x-2 ml-2 transform translate-y-1">
+          {techIcons.map(({ src, alt, label }, idx) => (
+            <div key={idx} className="relative group">
+              <Image
+                src={src}
+                alt={alt}
+                width={20}
+                height={20}
+                objectFit="contain"
+                className={"rounded-full"}
+              />
+              <div className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100 bg-black text-white text-xs rounded px-2 py-1 -top-8">
+                {label}
+              </div>
             </div>
-          </div>
-          <div className="relative group">
-            <Image
-              src="/next.png"
-              alt="Next.js icon"
-              width={20}
-              height={20}
-              objectFit="contain"
-              className=""
-            />
-            <div className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100 bg-black text-white text-xs rounded px-2 py-1 -top-8">
-              Next.js
-            </div>
-          </div>
-
-          <div className="relative group">
-            <Image
-              src="/typescript.png"
-              alt="TypeScript icon"
-              width={20}
-              height={20}
-              objectFit="cover"
-              className="rounded-full"
-            />
-            <div className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100 bg-black text-white text-xs rounded px-2 py-1 -top-8">
-              TypeScript
-            </div>
-          </div>
-
-          <div className="relative group">
-            <Image
-              src="/tw.png"
-              alt="Tailwind CSS icon"
-              width={20}
-              height={20}
-              objectFit="contain"
-              className=""
-            />
-            <div className="absolute left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 delay-100 bg-black text-white text-xs rounded px-2 py-1 -top-8">
-              Tailwind CSS
-            </div>
-          </div>
+          ))}
         </div>
       </span>
-    </motion.p>
+    </motion.div>
   );
 }
